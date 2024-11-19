18139705b61facee76d742e2f06bbd1154bc5c19
OrkunDemir5 <orkun.d1999@gmail.com>
2024-11-20 01:39:06 +0300

    update

---
+++ Pratik-Asp.Net/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Pratik_Asp.Net.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Pratik-Asp.Net/Views/Home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pratik_Asp.Net.Views.Home
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
